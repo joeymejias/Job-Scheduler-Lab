1af6406169217305f5d3b60428026303d5572160
Joey <joeymejias@gmail.com>
2016-08-12 14:09:34 -0400

    data thing

--- JobSchedulerLab/app/src/main/java/com/joeymejias/jobschedulerlab/MainActivity.java
+++ JobSchedulerLab/app/src/main/java/com/joeymejias/jobschedulerlab/MainActivity.java
@@ -5,13 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DataSingleton.DataChangeListener{
+
+    TextView oldText, newText, evenNewerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        oldText = (TextView) findViewById(R.id.textview_1);
+        newText = (TextView) findViewById(R.id.textview_2);
+        evenNewerText = (TextView) findViewById(R.id.textview_3);
+
+        oldText.setText(DataSingleton.getInstance().getMyText());
+        newText.setText(DataSingleton.getInstance().getMyText());
+        evenNewerText.setText(DataSingleton.getInstance().getMyText());
 
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onDataChanged(String oldText) {
+        Toast.makeText(MainActivity.this, "Data changed", Toast.LENGTH_SHORT).show();
+    }
 }
 
